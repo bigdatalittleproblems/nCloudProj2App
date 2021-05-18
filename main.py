78a0a41cba7da5f1f554c9e9e2f48c7cb3d18c62
Christian <christianramirez.datasci@gmail.com>
2021-05-17 22:50:38 -0700

    test redis

--- main.py
+++ main.py
@@ -5,27 +5,27 @@ import os
 redishost = os.environ['redishost']
 app = Flask(__name__)
 
+testbool=False
+if testbool:
+    @app.route('/')
+    def hello():
+        # redis.incr('hits')
+        return 'This Compose/Flask demo has been viewed {} time(s). Created by Christian Ramirez'.format(123)
+    @app.route('/test')
+    def test():
+        # redis.incr('hits')
+        return 'This is the path "/test" and it has been viewed {} time(s). Created by Christian Ramirez'.format(123)
+else:
+    redis = Redis(host=redishost, port=6379)
+    @app.route('/')
+    def hello():
+        redis.incr('hits')
+        return 'This Compose/Flask demo has been viewed {} time(s). Created by Christian Ramirez'.format(redis.get('hits'))
 
-@app.route('/')
-def hello():
-    # redis.incr('hits')
-    return 'This Compose/Flask demo has been viewed {} time(s). Created by Christian Ramirez'.format(123)
-
-@app.route('/test')
-def test():
-    # redis.incr('hits')
-    return 'This is the path "/test" and it has been viewed {} time(s). Created by Christian Ramirez'.format(123)
-
-# redis = Redis(host=redishost, port=6379)
-# @app.route('/')
-# def hello():
-#     redis.incr('hits')
-#     return 'This Compose/Flask demo has been viewed {} time(s). Created by Christian Ramirez'.format(redis.get('hits'))
-
-# @app.route('/test')
-# def test():
-#     redis.incr('hits')
-#     return 'This is the path "/test" and it has been viewed {} time(s). Created by Christian Ramirez'.format(redis.get('hits'))
+    @app.route('/test')
+    def test():
+        redis.incr('hits')
+        return 'This is the path "/test" and it has been viewed {} time(s). Created by Christian Ramirez'.format(redis.get('hits'))
 
 
 if __name__ == "__main__":
