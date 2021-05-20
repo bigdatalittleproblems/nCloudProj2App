2cebfc7c92dd3dfd33d490038c2b7721fcb7c54d
Christian <christianramirez.datasci@gmail.com>
2021-05-20 16:10:40 -0700

    Jenkins Taging of Docker and Helm Removed Install

--- deployhelm.sh
+++ deployhelm.sh
@@ -1,12 +1,14 @@
 #!/bin/sh
-DEPLOYED=$(helm list | grep deployed | wc -l)
-PACKAGE=ncloud
-if [ $DEPLOYED = 0 ]
-then
-    echo "Helm Chart is installing for the first time"
-    echo "helm install $PACKAGE cramirez-ncloud --set image.tag=${env.BUILD_ID}"
-else
-    echo "Helm Chart is updating to version ${env.BUILD_ID}"
-    echo "helm upgrade $PACKAGE cramirez-ncloud --set image.tag=${env.BUILD_ID}"
-fi
-echo "Deployed Helm Chart"
+echo "Helm Chart is installing for the first time"
+echo "helm install ncloud cramirez-ncloud --set image.tag=latest"
+# DEPLOYED=$(helm list | grep deployed | wc -l)
+# PACKAGE=ncloud
+# if [ $DEPLOYED = 0 ]
+# then
+#     echo "Helm Chart is installing for the first time"
+#     echo "helm install $PACKAGE cramirez-ncloud --set image.tag=${env.BUILD_ID}"
+# else
+#     echo "Helm Chart is updating to version ${env.BUILD_ID}"
+#     echo "helm upgrade $PACKAGE cramirez-ncloud --set image.tag=${env.BUILD_ID}"
+# fi
+# echo "Deployed Helm Chart"
--- dockerapp/main.py
+++ dockerapp/main.py
@@ -5,27 +5,16 @@ import os
 redishost = os.environ['redishost']
 app = Flask(__name__)
 
-testbool=False
-if testbool:
-    @app.route('/')
-    def hello():
-        # redis.incr('hits')
-        return 'This Compose/Flask demo has been viewed {} time(s). Created by Christian Ramirez'.format(123)
-    @app.route('/test')
-    def test():
-        # redis.incr('hits')
-        return 'This is the path "/test" and it has been viewed {} time(s). Created by Christian Ramirez'.format(123)
-else:
-    redis = Redis(host="projectcache.dgmfid.0001.use1.cache.amazonaws.com", port=6379)
-    @app.route('/')
-    def hello():
-        # redis.incr('hits')
-        output="<!DOCTYPE html><html><body><h1>Christian Ramirez Test Page</h1><p>got to /test to check the output of redis </p></body></html>"
-        return output
-    @app.route('/test')
-    def test():
-        redis.incr('hits')
-        output="<!DOCTYPE html><html><body><h1>Christian Ramirez Test Page</h1><h2>Redis Backend Test Page</h2><p>This page has been viewed {} times</p></body></html>".format(int(redis.get('hits')))
-        return output
+redis = Redis(host="projectcache.dgmfid.0001.use1.cache.amazonaws.com", port=6379)
+@app.route('/')
+def hello():
+    # redis.incr('hits')
+    output="<!DOCTYPE html><html><body><h1>Christian Ramirez Test Page</h1><h2>Version:1.0</h2><p>got to /test to check the output of redis </p></body></html>"
+    return output
+@app.route('/test')
+def test():
+    redis.incr('hits')
+    output="<!DOCTYPE html><html><body><h1>Christian Ramirez Test Page</h1><h2>Redis Backend Test Page</h2><p>This page has been viewed {} times</p></body></html>".format(int(redis.get('hits')))
+    return output
 if __name__ == "__main__":
     app.run(host="0.0.0.0", debug=True,port='5000')
